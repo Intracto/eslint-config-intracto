7db5f62b663eb15bc5cc82505d8f0b50d0c660d0
yulian.alexeyev <yulian.alexeyev@intracto.com>
2019-12-09 11:27:48 +0100

    update order of extends, move airbnb higher

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 module.exports = {
-  extends: ['eslint-config-airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    "eslint:recommended",
+    "eslint-config-airbnb-base",
+    "plugin:prettier/recommended"
+  ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   env: {
     browser: true,
     node: true,
-    es6: true,
-  },
-}
+    es6: true
+  }
+};
